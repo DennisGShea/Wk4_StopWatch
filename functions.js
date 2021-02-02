8e04481b2117a580d086dca6eae2fae2a2accfef
Dennis G Shea <DennisGSheaPhD@gmail.com>
2021-02-02 01:29:43 -0500

    working on intervalStop/clear

--- functions.js
+++ functions.js
@@ -1,13 +1,18 @@
-function clickHandler(Parm1,Parm2) {
-    console.log("pressed")
-    let a = 0 
-    var intervalID = window.setInterval(myCallback, 2000);
+let ID = 0
+
+function clickHandler(ID) {
+    console.log("pressed") 
+    let a = 0
+    var intervalID = window.setInterval(myCallback, 1250);
         function myCallback()
             {
-               
                 console.log(a++)
-            //console.log(a);
-            //console.log(b);
             }
-     //console.log(intervalID)
+     ID = intervalID       
+     console.log("once",intervalID,a)
+}   
+
+function clickStopHandler(ID) {
+    console.log("pressed_stopped")
+    window.clearInterval(ID)
 }   
